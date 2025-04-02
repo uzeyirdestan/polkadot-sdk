e34c5b4445053274fa5ce48ebec95d239a60793d
github-action-benchmark <github@users.noreply.github.com>
2025-04-02 12:40:10 +0000

    add notifications_protocol (cargo) benchmark result for 3766467f5adc670ff4f71e2ec9f27185fdcf97ca

--- bench/notifications_protocol/data.js
+++ bench/notifications_protocol/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1743541074075,
+  "lastUpdate": 1743597610640,
   "repoUrl": "https://github.com/paritytech/polkadot-sdk",
   "entries": {
     "notifications_protocol": [
@@ -52607,6 +52607,198 @@ window.BENCHMARK_DATA = {
             "name": "notifications_protocol/litep2p/with_backpressure/16MB",
             "value": 2282518824,
             "range": "± 43712069",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "adrian@parity.io",
+            "name": "Adrian Catangiu",
+            "username": "acatangiu"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": false,
+          "id": "3766467f5adc670ff4f71e2ec9f27185fdcf97ca",
+          "message": "pallet-xcm: add support to authorize aliases (#6336)\n\nAdd calls to pallet-xcm for adding and removing authorization for a\ncertain `aliaser` location to alias into the caller `origin`.\n\n`pallet-xcm` also exposes an `AuthorizedAliases` filter implementation\nusable with `xcm_executor::Config::Aliasers` filter to easily allow\nruntimes to plug in the explicitly authorized aliases using the calls\nabove.\n\nUsually useful to allow your local account to be aliased into from a\nremote location also under your control (like your account on another\nchain).\n\nFor example, `Alice` on `Para42` can do _something_ on Asset Hub without\nhaving to transfer fees from `Para42`, but instead use her local Asset\nHub account:\n```rust\n// called by Alice on Para42\npallet_xcm::send(\n\tLocation::new(1, Parachain(1000)),\n\tXcm(vec![\n\t\tAliasOrigin(AliceOnAH),\n\t\tWithdrawAsset(fees),\n\t\tPayFees(fees),\n\t\tDoWhatever\n\t])\n);\n```\n\nPart of [Empowered cross-chain\norigins](https://github.com/paritytech/polkadot-sdk/issues/6054).\n\nFixes [XCM: Arbitrary Origin Aliases\n#722](https://github.com/paritytech/polkadot-sdk/issues/722)\n\n---------\n\nCo-authored-by: command-bot <>\nCo-authored-by: Francisco Aguirre <franciscoaguirreperez@gmail.com>\nCo-authored-by: cmd[bot] <41898282+github-actions[bot]@users.noreply.github.com>",
+          "timestamp": "2025-04-02T11:37:32Z",
+          "tree_id": "ea8ba5955d28d3a7a545797fd358679c614812c3",
+          "url": "https://github.com/paritytech/polkadot-sdk/commit/3766467f5adc670ff4f71e2ec9f27185fdcf97ca"
+        },
+        "date": 1743597593387,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "notifications_protocol/libp2p/serially/64B",
+            "value": 4217505,
+            "range": "± 27672",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "notifications_protocol/libp2p/with_backpressure/64B",
+            "value": 296865,
+            "range": "± 4284",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "notifications_protocol/libp2p/serially/512B",
+            "value": 4346206,
+            "range": "± 56219",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "notifications_protocol/libp2p/with_backpressure/512B",
+            "value": 392713,
+            "range": "± 4213",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "notifications_protocol/libp2p/serially/4KB",
+            "value": 5104758,
+            "range": "± 40093",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "notifications_protocol/libp2p/with_backpressure/4KB",
+            "value": 870947,
+            "range": "± 11974",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "notifications_protocol/libp2p/serially/64KB",
+            "value": 10351375,
+            "range": "± 105923",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "notifications_protocol/libp2p/with_backpressure/64KB",
+            "value": 4936135,
+            "range": "± 258769",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "notifications_protocol/libp2p/serially/256KB",
+            "value": 45685164,
+            "range": "± 564834",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "notifications_protocol/libp2p/with_backpressure/256KB",
+            "value": 37961584,
+            "range": "± 447211",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "notifications_protocol/libp2p/serially/2MB",
+            "value": 367407951,
+            "range": "± 5801746",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "notifications_protocol/libp2p/with_backpressure/2MB",
+            "value": 292543943,
+            "range": "± 2012833",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "notifications_protocol/libp2p/serially/16MB",
+            "value": 2561217573,
+            "range": "± 14653549",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "notifications_protocol/libp2p/with_backpressure/16MB",
+            "value": 2254433270,
+            "range": "± 10555759",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "notifications_protocol/litep2p/serially/64B",
+            "value": 3102487,
+            "range": "± 35232",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "notifications_protocol/litep2p/with_backpressure/64B",
+            "value": 1423346,
+            "range": "± 12922",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "notifications_protocol/litep2p/serially/512B",
+            "value": 3181078,
+            "range": "± 40714",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "notifications_protocol/litep2p/with_backpressure/512B",
+            "value": 1500439,
+            "range": "± 14161",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "notifications_protocol/litep2p/serially/4KB",
+            "value": 3777668,
+            "range": "± 32596",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "notifications_protocol/litep2p/with_backpressure/4KB",
+            "value": 1848450,
+            "range": "± 9503",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "notifications_protocol/litep2p/serially/64KB",
+            "value": 7910324,
+            "range": "± 66571",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "notifications_protocol/litep2p/with_backpressure/64KB",
+            "value": 5006246,
+            "range": "± 79577",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "notifications_protocol/litep2p/serially/256KB",
+            "value": 35053539,
+            "range": "± 402861",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "notifications_protocol/litep2p/with_backpressure/256KB",
+            "value": 37150879,
+            "range": "± 494552",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "notifications_protocol/litep2p/serially/2MB",
+            "value": 337266447,
+            "range": "± 4022325",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "notifications_protocol/litep2p/with_backpressure/2MB",
+            "value": 296445111,
+            "range": "± 7721076",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "notifications_protocol/litep2p/serially/16MB",
+            "value": 2615382873,
+            "range": "± 9164108",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "notifications_protocol/litep2p/with_backpressure/16MB",
+            "value": 2441027148,
+            "range": "± 52781505",
             "unit": "ns/iter"
           }
         ]
