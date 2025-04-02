333c296e4ebb6d507f05c59209828676a69a138a
github-action-benchmark <github@users.noreply.github.com>
2025-04-02 12:49:22 +0000

    add availability-recovery-regression-bench (customSmallerIsBetter) benchmark result for 3766467f5adc670ff4f71e2ec9f27185fdcf97ca

--- bench/availability-recovery-regression-bench/data.js
+++ bench/availability-recovery-regression-bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1743541656108,
+  "lastUpdate": 1743598161921,
   "repoUrl": "https://github.com/paritytech/polkadot-sdk",
   "entries": {
     "availability-recovery-regression-bench": [
@@ -50635,6 +50635,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "test-environment",
             "value": 0.20259447253333335,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "adrian@parity.io",
+            "name": "Adrian Catangiu",
+            "username": "acatangiu"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": false,
+          "id": "3766467f5adc670ff4f71e2ec9f27185fdcf97ca",
+          "message": "pallet-xcm: add support to authorize aliases (#6336)\n\nAdd calls to pallet-xcm for adding and removing authorization for a\ncertain `aliaser` location to alias into the caller `origin`.\n\n`pallet-xcm` also exposes an `AuthorizedAliases` filter implementation\nusable with `xcm_executor::Config::Aliasers` filter to easily allow\nruntimes to plug in the explicitly authorized aliases using the calls\nabove.\n\nUsually useful to allow your local account to be aliased into from a\nremote location also under your control (like your account on another\nchain).\n\nFor example, `Alice` on `Para42` can do _something_ on Asset Hub without\nhaving to transfer fees from `Para42`, but instead use her local Asset\nHub account:\n```rust\n// called by Alice on Para42\npallet_xcm::send(\n\tLocation::new(1, Parachain(1000)),\n\tXcm(vec![\n\t\tAliasOrigin(AliceOnAH),\n\t\tWithdrawAsset(fees),\n\t\tPayFees(fees),\n\t\tDoWhatever\n\t])\n);\n```\n\nPart of [Empowered cross-chain\norigins](https://github.com/paritytech/polkadot-sdk/issues/6054).\n\nFixes [XCM: Arbitrary Origin Aliases\n#722](https://github.com/paritytech/polkadot-sdk/issues/722)\n\n---------\n\nCo-authored-by: command-bot <>\nCo-authored-by: Francisco Aguirre <franciscoaguirreperez@gmail.com>\nCo-authored-by: cmd[bot] <41898282+github-actions[bot]@users.noreply.github.com>",
+          "timestamp": "2025-04-02T11:37:32Z",
+          "tree_id": "ea8ba5955d28d3a7a545797fd358679c614812c3",
+          "url": "https://github.com/paritytech/polkadot-sdk/commit/3766467f5adc670ff4f71e2ec9f27185fdcf97ca"
+        },
+        "date": 1743598144593,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Received from peers",
+            "value": 307203,
+            "unit": "KiB"
+          },
+          {
+            "name": "Sent to peers",
+            "value": 1.6666666666666665,
+            "unit": "KiB"
+          },
+          {
+            "name": "test-environment",
+            "value": 0.20697689390000001,
+            "unit": "seconds"
+          },
+          {
+            "name": "availability-recovery",
+            "value": 11.488408607299998,
             "unit": "seconds"
           }
         ]
