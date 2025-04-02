695b0bcfed3deeb0b4b0be87a188912dc092704c
github-action-benchmark <github@users.noreply.github.com>
2025-04-02 12:40:35 +0000

    add request_response_protocol (cargo) benchmark result for 3766467f5adc670ff4f71e2ec9f27185fdcf97ca

--- bench/request_response_protocol/data.js
+++ bench/request_response_protocol/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1743541099235,
+  "lastUpdate": 1743597634868,
   "repoUrl": "https://github.com/paritytech/polkadot-sdk",
   "entries": {
     "request_response_protocol": [
@@ -29159,6 +29159,114 @@ window.BENCHMARK_DATA = {
             "name": "request_response_protocol/litep2p/serially/16MB",
             "value": 2466372800,
             "range": "± 17221843",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "adrian@parity.io",
+            "name": "Adrian Catangiu",
+            "username": "acatangiu"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": false,
+          "id": "3766467f5adc670ff4f71e2ec9f27185fdcf97ca",
+          "message": "pallet-xcm: add support to authorize aliases (#6336)\n\nAdd calls to pallet-xcm for adding and removing authorization for a\ncertain `aliaser` location to alias into the caller `origin`.\n\n`pallet-xcm` also exposes an `AuthorizedAliases` filter implementation\nusable with `xcm_executor::Config::Aliasers` filter to easily allow\nruntimes to plug in the explicitly authorized aliases using the calls\nabove.\n\nUsually useful to allow your local account to be aliased into from a\nremote location also under your control (like your account on another\nchain).\n\nFor example, `Alice` on `Para42` can do _something_ on Asset Hub without\nhaving to transfer fees from `Para42`, but instead use her local Asset\nHub account:\n```rust\n// called by Alice on Para42\npallet_xcm::send(\n\tLocation::new(1, Parachain(1000)),\n\tXcm(vec![\n\t\tAliasOrigin(AliceOnAH),\n\t\tWithdrawAsset(fees),\n\t\tPayFees(fees),\n\t\tDoWhatever\n\t])\n);\n```\n\nPart of [Empowered cross-chain\norigins](https://github.com/paritytech/polkadot-sdk/issues/6054).\n\nFixes [XCM: Arbitrary Origin Aliases\n#722](https://github.com/paritytech/polkadot-sdk/issues/722)\n\n---------\n\nCo-authored-by: command-bot <>\nCo-authored-by: Francisco Aguirre <franciscoaguirreperez@gmail.com>\nCo-authored-by: cmd[bot] <41898282+github-actions[bot]@users.noreply.github.com>",
+          "timestamp": "2025-04-02T11:37:32Z",
+          "tree_id": "ea8ba5955d28d3a7a545797fd358679c614812c3",
+          "url": "https://github.com/paritytech/polkadot-sdk/commit/3766467f5adc670ff4f71e2ec9f27185fdcf97ca"
+        },
+        "date": 1743597617798,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "request_response_protocol/libp2p/serially/64B",
+            "value": 18310002,
+            "range": "± 162708",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "request_response_protocol/libp2p/serially/512B",
+            "value": 18671578,
+            "range": "± 219897",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "request_response_protocol/libp2p/serially/4KB",
+            "value": 19969712,
+            "range": "± 223413",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "request_response_protocol/libp2p/serially/64KB",
+            "value": 24708252,
+            "range": "± 120810",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "request_response_protocol/libp2p/serially/256KB",
+            "value": 59488578,
+            "range": "± 935144",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "request_response_protocol/libp2p/serially/2MB",
+            "value": 384570367,
+            "range": "± 7187257",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "request_response_protocol/libp2p/serially/16MB",
+            "value": 2878041133,
+            "range": "± 21763527",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "request_response_protocol/litep2p/serially/64B",
+            "value": 15530385,
+            "range": "± 146721",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "request_response_protocol/litep2p/serially/512B",
+            "value": 15599743,
+            "range": "± 121977",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "request_response_protocol/litep2p/serially/4KB",
+            "value": 16248818,
+            "range": "± 210702",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "request_response_protocol/litep2p/serially/64KB",
+            "value": 20583400,
+            "range": "± 319565",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "request_response_protocol/litep2p/serially/256KB",
+            "value": 53382390,
+            "range": "± 762635",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "request_response_protocol/litep2p/serially/2MB",
+            "value": 329751404,
+            "range": "± 5495937",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "request_response_protocol/litep2p/serially/16MB",
+            "value": 2564037537,
+            "range": "± 40192373",
             "unit": "ns/iter"
           }
         ]
